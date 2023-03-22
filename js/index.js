152ece0a106106bf87244bf130d93285dd7f9335
Kyan <k.gerritsma@sintlucasedu.nl>
2023-03-22 16:39:38 +0100

    asd

--- js/index.js
+++ js/index.js
@@ -1,5 +1,6 @@
 document.getElementById("mainTitle").innerText="point and click adventure";
 
+const offsetCharacter = 16;
 const gameWindow = document.getElementById("gameWindow");
 const mainCharacter = document.getElementById("mainCharacter");
 
@@ -7,9 +8,22 @@ gameWindow.onclick = function(e){
     var rect = gameWindow.getBoundingClientRect();
     var x = e.clientX - rect.left;
     var y = e.clientY - rect.top;
-    mainCharacter.style.left = x+"px";
-    mainCharacter.style.top = y+"px";
 
+    mainCharacter.style.left = x - offsetCharacter +"px";
+    mainCharacter.style.top = y - offsetCharacter +"px";
 
-    console.log(x +" "+ y);
+    switch(e.target.id){
+        case "statue":
+        console.log("the statue of liberty");
+        break
+
+        case "grave1":
+        console.log("someone is resting here");
+        break
+        default:
+            console.log("nothing to do...");
+        break
+    }
+
+    console.log(e.target.id);
 }
